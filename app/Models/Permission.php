6d508b3ed3c9747087b2a1706a2500d6c31fcbba
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-12-14 10:25:57 +0000

    Eloquent relationship

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Permission extends Model
 {
     use HasFactory;
+
+    /** @var array<int, string> */
+    protected array $fillable = [
+        'name',
+    ];
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class, 'role_has_permissions');
+    }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
     use HasFactory;
+
+    /** @var array<int, string> */
+    protected array $fillable = [
+        'name',
+    ];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class, 'role_has_permissions');
+    }
 }
