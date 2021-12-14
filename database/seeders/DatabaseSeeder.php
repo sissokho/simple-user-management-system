66116fa9f436669ccec8f68cc07dce224bb9f5bc
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-12-14 12:09:20 +0000

    Seed database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Create Permissions
+        Permission::create(['name' => 'create user']);
+        Permission::create(['name' => 'edit user']);
+        Permission::create(['name' => 'view user']);
+        Permission::create(['name' => 'delete user']);
+
+        // Create roles and assign permissions
+        $superAdminRole = Role::create(['name' => 'super-admin']); // Has all permissions. see AuthServiceProvider
+
+        $adminRole = Role::create(['name' => 'admin']);
+        $adminRole->givePermissionTo('create user');
+        $adminRole->givePermissionTo('edit user');
+        $adminRole->givePermissionTo('view user');
+        $adminRole->givePermissionTo('delete user');
+
+        $userRole = Role::create(['name' => 'user']);
+
+        // Create a superadmin
+        $superAdmin = User::factory()->create([
+            'name' => 'Mouhamadou Moustapha',
+            'email' => 'siskomouhamed@gmail.com',
+        ]);
+
+        $superAdmin->assignRole($superAdminRole);
     }
 }
