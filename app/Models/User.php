480e5a7d409726ae9ed28519dcdfd198600decd3
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-12-14 10:27:23 +0000

    Add some type hinting

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
+    protected array $fillable = [
         'name',
         'email',
         'password',
@@ -30,7 +30,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $hidden = [
+    protected array $hidden = [
         'password',
         'remember_token',
     ];
@@ -40,7 +40,7 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [
+    protected array  $casts = [
         'email_verified_at' => 'datetime',
     ];
 
