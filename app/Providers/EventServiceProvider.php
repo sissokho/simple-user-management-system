7752f7d18d1b42b89daf8d7a13045ef2386d9228
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-12-15 14:44:09 +0000

    Send a password reset link to the newly created user

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Events\UserCreated;
+use App\Listeners\MarkEmailAsVerified;
+use App\Listeners\SendPasswordResetLink;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,6 +21,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        UserCreated::class => [
+            SendPasswordResetLink::class,
         ],
     ];
 
