e294512cf2b491857c306bd97976bc2113e634cb
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-12-16 12:57:45 +0000

    List all users

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -10,6 +12,17 @@ class DashboardController extends Controller
 {
     public function __invoke(): View
     {
-        return view('dashboard');
+        $users = User::query()
+            ->with('role')
+            ->where('id', '<>', auth()->id())
+            ->whereRelation('role', 'name', '<>', 'super-admin')
+            ->when(auth()->user()->hasRole('admin'), function (Builder $query) {
+                $query->whereRelation('role', 'name', '<>', 'admin');
+            })
+            ->paginate(5);
+
+        return view('dashboard', [
+            'users' => $users
+        ]);
     }
 }
